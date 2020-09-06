8d0b069e355083f6e4a968b9ca901194c42a3c0c
guidofv <guidovitale@gmail.com>
2020-09-06 13:57:20 -0300

    Agregadas todos los avances salvo los que requieren hacer destructuring dentro de cada elemento de un array

--- index.js
+++ index.js
@@ -3,18 +3,10 @@ const fs = require('fs');
 
 // Modulos creados
 
-const movies = require('./modulos/movies');
+let movies = require('./modulos/movies');
 const faqs = require('./modulos/faqs');
 const theaters = require('./modulos/theaters');
 
-function nombresPelis(){
-	let nomPelis = [];
-	for(i=0; i<movies.length; i++){
-		nomPelis.push(' '+movies[i].original_title);
-	}
-	return(nomPelis.sort());
-    }	    
-    nombresPelis();
 	
 // Servidor
 http.createServer((req, res) => {
@@ -24,32 +16,93 @@ http.createServer((req, res) => {
 	switch (req.url) {
 		// Home
 		case '/':
-			res.end(`Título​: ​Bienvenidos a DH Movies el mejor sitio para encontrar las mejores
-            películas, incluso mucho mejor que Netflix, Cuevana y PopCorn​.
-            b. Total de películas en cartelera​: ${ movies.length }
-            c. Listados de películas:${nombresPelis()}
-            d. Pié de página​: ​Recordá que podés visitar las secciones:
-            i. En Cartelera
-            ii. Más Votadas
-            iii. Sucursales
-            iv. Contacto
-            v. Preguntas Frecuentes`);
+			movies = movies.sort(function(a,b){
+				if (a.title > b.title){
+					return 1;
+				} else {
+					if (a.title < b.title){
+						return -1;
+					} else {
+						return 0;
+					}
+				}
+			});
+
+			let titlesAZ = ``
+			for(let i = 0; i<movies.length; i++){
+			titlesAZ += `<li> ${movies[i].title}`;
+}
+
+			let contenido = `
+			Bienvenidos a DH Movies el mejor sitio para encontrar las mejores
+			películas, incluso mucho mejor que Netflix, Cuevana y PopCorn​.<hr>
+			Total de películas en cartelera: ${movies.length}<br>
+			Listados de películas: ${titlesAZ}<br>
+			<br>
+			Recordá que podés visitar las secciones:
+            i. <a href=./en-cartelera>En Cartelera</a>
+            ii. <a href=./mas-votadas>Más Votadas</a>
+            iii. <a href=./sucursales>Sucursales</a> 
+            iv. <a href=./contacto>Contacto</a>
+            v. <a href=./preguntas-frecuentes>Preguntas Frecuentes</a>);
+			`
+
+			res.end(contenido);
 			break;
+
 		// En cartelera
 		case '/en-cartelera':
-			res.end('En cartelera');
+	
+			res.end(`
+			<b>En Cartelera</b>
+			Total de películas : ${movies.length} <br>
+			Título y reseña:
+			c. Listados de películas. Para cada película mostrar:
+			`);
 			break;
 		case '/mas-votadas':
-			res.end('Más Votadas');
+				
+			let valoraciones = movies.map(function(elemento){
+				return elemento.vote_average;
+			})
+			
+			let rankingsAltos = valoraciones.filter(function(numero){
+				return numero >= 7;
+			})
+
+			let sumaRankingsAltos = rankingsAltos.reduce(function(estado,numero){
+				return estado + numero;
+			})
+
+			let promedioRatingsAltos = sumaRankingsAltos/rankingsAltos.length;
+
+			res.end(`
+			Más Votadas <br>
+			Cantidad: ${rankingsAltos.length} <br>
+			Rating promedio: ${promedioRatingsAltos}`);
 			break;
 		case '/sucursales':
-			res.end('Sucursales');
+			res.end(`
+			Nuestras salas:<br>
+			Total de salas: ${theaters.length}`);
 			break;
 		case '/contacto':
-			res.end('Contacto');
+			res.end(`
+			Contáctanos <br>
+			¿Tenés algo para contarnos? Nos encanta escuchar a nuestros
+clientes. Si deseas contactarnos podés escribirnos al siguiente email:
+dhmovies@digitalhouse.com o en las redes sociales. Envianos tu consulta,
+sugerencia o reclamo y será respondido a la brevedad posible. Recordá que
+también podes consultar la sección de Preguntas Frecuentes para obtener
+respuestas inmediatas a los problemas más comunes.`);
 			break;
 		case '/preguntas-frecuentes':
-			res.end('Preguntas Frecuentes');
+			res.end(`
+			Preguntas Frecuentes<br>
+			Total de preguntas: ${faqs.length} <br>
+			Listados de preguntas:
+		
+			`);
 			break;
 		default:
 			res.end('404 not found')
